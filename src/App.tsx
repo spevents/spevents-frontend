d7679391b6e45943517e6b9c6f85872c2d818e0e
fbablu <fardeeneb@gmail.com>
2024-12-07 22:31:56 -0600

    added index for layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,34 @@
 // src/App.tsx
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { CameraPage, GalleryPage, QRCodePage, PhotoReviewPage, SlideshowPage, VenuePage } from './pages';
+import { CameraPage, GalleryPage, QRCodePage, PhotoReviewPage, SlideshowPage } from './pages';
 import { NgrokProvider } from './contexts/NgrokContext';
-import { SessionProvider } from './contexts/SessionContext';
 import { LandingPage, ProductPage, ExamplesPage } from './pages/landing';
-import { HostSetup } from './components/HostSetup';
-import { JoinSession } from './components/JoinSession';
+import { DemoLayout, MainLayout } from './layouts';
+
 
 export default function App() {
   return (
     <NgrokProvider>
-      <SessionProvider>
-        <BrowserRouter>
-          <Routes>
-            {/* Marketing/Landing Pages */}
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/product" element={<ProductPage />} />
-            <Route path="/examples" element={<ExamplesPage />} />
-            
-            {/* Host Routes */}
-            <Route path="/host" element={<HostSetup />} />
-            <Route path="/venue" element={<VenuePage />} />
-            <Route path="/gallery" element={<GalleryPage />} />
-            <Route path="/slideshow" element={<SlideshowPage />} />
-            
-            {/* Guest Routes */}
-            <Route path="/join" element={<JoinSession />} />
-            <Route path="/join/:code" element={<JoinSession />} />
-            <Route path="/camera" element={<CameraPage />} />
-            <Route path="/review" element={<PhotoReviewPage />} />
-            
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </SessionProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* Marketing/Landing Pages */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/product" element={<ProductPage />} />
+          <Route path="/examples" element={<ExamplesPage />} />
+          
+          {/* Demo Section - Nested Routes */}
+          <Route path="/demo" element={<DemoLayout />}>
+            <Route index element={<MainLayout />} />
+            <Route path="qr" element={<QRCodePage />} />
+            <Route path="camera" element={<CameraPage />} />
+            <Route path="gallery" element={<GalleryPage />} />
+            <Route path="slideshow" element={<SlideshowPage />} />
+            <Route path="review" element={<PhotoReviewPage />} />
+          </Route>
+          
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
     </NgrokProvider>
   );
 }
