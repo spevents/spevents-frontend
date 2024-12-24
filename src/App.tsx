1a929a3b3fcfb0cc33c0e3d3f70885c7d4804129
fbablu <fardeeneb@gmail.com>
2024-12-23 20:07:12 -0500

    removed copies

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { CameraPage, GalleryPage, QRCodePage, PhotoReviewPage, SlideshowPage } from './pages';
 import { NgrokProvider } from './contexts/NgrokContext';
-import { LandingPage, ProductPage, ExamplesPage } from './pages/landing';
-import { DemoLayout, MainLayout } from './layouts';
+import { MainLayout } from './layouts';
 
 
 export default function App() {
@@ -11,21 +10,12 @@ export default function App() {
     <NgrokProvider>
       <BrowserRouter>
         <Routes>
-          {/* Marketing/Landing Pages */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/product" element={<ProductPage />} />
-          <Route path="/examples" element={<ExamplesPage />} />
-          
-          {/* Demo Section - Nested Routes */}
-          <Route path="/demo" element={<DemoLayout />}>
-            <Route index element={<MainLayout />} />
-            <Route path="qr" element={<QRCodePage />} />
-            <Route path="camera" element={<CameraPage />} />
-            <Route path="gallery" element={<GalleryPage />} />
-            <Route path="slideshow" element={<SlideshowPage />} />
-            <Route path="review" element={<PhotoReviewPage />} />
-          </Route>
-          
+          <Route path="/" element={<MainLayout />} />
+          <Route path="qr" element={<QRCodePage />} />
+          <Route path="camera" element={<CameraPage />} />
+          <Route path="gallery" element={<GalleryPage />} />
+          <Route path="slideshow" element={<SlideshowPage />} />
+          <Route path="review" element={<PhotoReviewPage />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
--- src/pages/GalleryPage copy.tsx
+++
@@ -1,9 +0,0 @@
-import { PhotoGallery } from '../components';
-
-export default function GalleryPage() {
-  return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-800">
-      <PhotoGallery />
-    </div>
-  );
-}
