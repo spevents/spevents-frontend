d669b4c403138b658de26445ec3c378a78931ebd
spevents <spevents.party@gmail.com>
2025-01-18 15:17:36 -0600

    removed qrcodepage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,4 @@
 export { default as CameraPage } from './CameraPage';
 export { default as GalleryPage } from './GalleryPage';
 export { default as PhotoReviewPage } from './PhotoReviewPage';
-export { default as QRCodePage } from './QRCodePage';
 export { default as SlideshowPage } from './SlideshowPage';
