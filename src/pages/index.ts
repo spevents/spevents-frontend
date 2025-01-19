b5d6f8885abf819a4bf47990637fe9ac00ce2b34
spevents <spevents.party@gmail.com>
2025-01-18 20:24:42 -0600

    added exit for camera interface page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
-export { default as CameraPage } from './CameraPage';
 export { default as GalleryPage } from './GalleryPage';
 export { default as PhotoReviewPage } from './PhotoReviewPage';
 export { default as SlideshowPage } from './SlideshowPage';
